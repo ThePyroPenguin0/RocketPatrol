de1988f646d3feccef258ac32df5d5d464896779
ThePyroPenguin0 <glipow@ucsc.edu>
2025-01-13 20:33:37 -0800

    first commit

---
+++ src/scenes/Play.js
@@ -0,0 +1,67 @@
+class Play extends Phaser.Scene
+{
+    constructor()
+    {
+        super('playScene');
+    }
+
+    create()
+    {
+        // Assets
+        this.starfield = this.add.tileSprite(0,0,640,480, "starfield").setOrigin(0,0);
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize*2, 0x00FF00).setOrigin(0,0);
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        
+        // Objects
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, "rocket").setOrigin(0.5, 0);        
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0)
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0)
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0)
+        
+        // Controls
+        keyFIRE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update()
+    {
+        this.starfield.tilePositionX -= 1;
+        this.p1Rocket.update();
+        this.ship01.update();
+        this.ship02.update();
+        this.ship03.update();
+
+        if(this.checkCollision(this.p1Rocket, this.ship03))
+        {
+            this.ship03.reset();
+            this.p1Rocket.reset();
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship02))
+        {
+            this.ship02.reset();
+            this.p1Rocket.reset();
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship01)) 
+        {
+            this.ship01.reset();
+            this.p1Rocket.reset();
+        }
+    }
+
+    checkCollision(rocket, ship) 
+    {
+        if (rocket.x < ship.x + ship.width && rocket.x + rocket.width > ship.x && rocket.y < ship.y + ship.height && rocket.height + rocket.y > ship. y)
+            {
+                return true
+            }
+            else
+            {
+                return false
+            }
+      }
+}
